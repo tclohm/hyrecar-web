a40dbd410529e04956058981567b8bad0f14323b
Taylor Lohman <taylor@Taylors-MBP.attlocal.net>
2021-06-05 13:12:47 -0700

    added increase and decrease

--- src/data/cars.js
+++ src/data/cars.js
@@ -2,7 +2,8 @@ import { make, model, year, vin, ratePerDay, maxMilesPerDay } from "../validatio
 const info = [
     { 
         id: 0, 
-        className: "md:w-1/2 md:bg-green-200 md:flex md:items-center md:justify-center md:mb-0 md:text-2xl", 
+
+        className: "md:w-1/2 flex justify-center items-center md:bg-green-200 md:text-2xl", 
         progress: "absolute h-1 top-0 appearance-none bg-green-200",
         name: "make", 
         question: "What's the make of your car?", 
@@ -10,7 +11,7 @@ const info = [
     },
     { 
         id: 12, 
-        className: "md:w-1/2 md:bg-blue-200 md:flex md:items-center md:justify-center md:text-2xl", 
+        className: "md:w-1/2 flex justify-center items-center md:bg-blue-200 md:text-2xl", 
         progress: "absolute h-1 top-0 appearance-none bg-green-200 animate-progress-1 w-1/12",
         name: "model", 
         question: "What's the model of your ", 
@@ -18,7 +19,7 @@ const info = [
     },
     { 
         id: 24, 
-        className: "md:w-1/2 md:bg-yellow-200 md:flex md:items-center md:justify-center md:mb-0 mb-44 md:text-xl", 
+        className: "md:w-1/2 flex justify-center items-center md:bg-yellow-200 md:text-xl", 
         progress: "absolute h-1 top-0 appearance-none bg-green-200 animate-progress-2 w-2/12",
         name: "year", 
         question: "What's the year of your ", 
@@ -26,7 +27,7 @@ const info = [
     },
     { 
         id: 36, 
-        className: "md:w-1/2 md:bg-purple-200 md:flex md:items-center md:justify-center md:mb-0 mb-44 md:text-xl", 
+        className: "md:w-1/2 flex justify-center items-center md:bg-purple-200 md:text-xl", 
         progress: "absolute h-1 top-0 appearance-none bg-green-200 animate-progress-3 w-3/12",
         name: "vin", 
         question: "What's vin number of your car?", 
@@ -34,7 +35,7 @@ const info = [
     },
     { 
         id: 48, 
-        className: "md:w-1/2 md:bg-pink-200 md:flex md:items-center md:justify-center md:mb-0 mb-44 md:text-xl", 
+        className: "md:w-1/2 flex justify-center items-center md:bg-pink-200 md:text-xl", 
         progress: "absolute h-1 top-0 appearance-none bg-green-200 animate-progress-4 w-4/12",
         name: "ratePerDay", 
         question: "How much will you charge per day to rent your car?", 
@@ -42,7 +43,7 @@ const info = [
     },
     { 
         id: 60, 
-        className: "md:w-1/2 md:bg-indigo-200 md:flex md:items-center md:justify-center md:mb-0 mb-44 md:text-xl", 
+        className: "md:w-1/2 flex justify-center items-center md:bg-indigo-200 md:text-xl", 
         progress: "absolute h-1 top-0 appearance-none bg-green-200 animate-progress-4 w-5/12",
         name: "maxMilesPerDay", 
         question: "What's the maximum amount of miles that is allowed on your car per day?", 
@@ -50,7 +51,7 @@ const info = [
     },
     { 
         id: 72, 
-        className: "md:w-1/2 md:bg-red-200 md:flex md:items-center md:justify-center md:mb-0 mb-44 md:text-xl", 
+        className: "md:w-1/2 flex justify-center items-center md:bg-red-200 md:text-xl", 
         progress: "absolute h-1 top-0 appearance-none bg-green-200 animate-progress-3 w-6/12",
         name: "amenities", 
         question: "What other features does your car have?", 
@@ -58,7 +59,7 @@ const info = [
     },
     { 
         id: 84, 
-        className: "md:w-1/2 md:bg-green-200 md:flex md:items-center md:justify-center md:mb-0 mb-44 md:text-2xl", 
+        className: "md:w-1/2 flex justify-center items-center md:bg-green-200 md:text-xl md:text-2xl", 
         progress: "absolute h-1 top-0 appearance-none bg-green-200 animate-progress-4 w-11/12",
         name: "photo", 
         question: "Add a photo of your car", 
